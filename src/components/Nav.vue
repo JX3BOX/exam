e5b195e9bada824d7a92aed20414789acc7da07f
iRuxu <rx6@qq.com>
2020-11-11 05:10:57 +0800

    update

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,25 +2,25 @@
     <nav class="m-nav">
         <h5 class="u-title">题库中心</h5>
         <div class="m-nav-group">
-            <router-link class="u-item" to="/qlist">
-                <i class="el-icon-s-order"></i>
-                <span>全部题目</span>
-            </router-link>
             <router-link class="u-item" to="/list">
                 <i class="el-icon-s-order"></i>
                 <span>全部试卷</span>
+            </router-link>
+            <router-link class="u-item" to="/qlist">
+                <i class="el-icon-s-order"></i>
+                <span>全部题目</span>
             </router-link>
         </div>
 
         <h5 class="u-title">参与贡献</h5>
         <div class="m-nav-group">
-            <a href="/dashboard/publish/#/exam/question" class="u-item" target="_blank">
-                <i class="el-icon-s-opportunity"></i>
-                <span>提交试题</span>
-            </a>
             <a href="/dashboard/publish/#/exam/paper" class="u-item" target="_blank">
                 <i class="el-icon-s-opportunity"></i>
                 <span>制作试卷</span>
+            </a>
+            <a href="/dashboard/publish/#/exam/question" class="u-item" target="_blank">
+                <i class="el-icon-s-opportunity"></i>
+                <span>提交试题</span>
             </a>
         </div>
     </nav>
